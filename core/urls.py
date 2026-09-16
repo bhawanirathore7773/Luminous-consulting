1c872e626525726b308eff64f4619619070b3537
Bhawani Singh <96902525+bhawanirathore7773@users.noreply.github.com>
2026-09-16 18:19:10 +0530

    Expose application health endpoint

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from core.views import ApproachView, HomeView
+from core.views import ApproachView, HomeView, healthz
 
 app_name = "core"
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("approach/", ApproachView.as_view(), name="approach"),
+    path("healthz/", healthz, name="healthz"),
 ]
